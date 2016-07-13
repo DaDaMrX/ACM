71122236a37e932a3bfe4850b2b0cf4811cbbfbd
DaDaMrX <dadamr_x@163.com>
2016-07-13 10:01:04 +0800

    updata

---
+++ HDU/HDU 1272 小希的迷宫 (并查集).cpp
@@ -0,0 +1,66 @@
+﻿#include <cstdio>
+#include <cstring>
+#define N 100010
+int fa[N], rank[N];
+bool flag[N];
+void init(int n)
+{
+	for (int i = 1; i <= n; i++) fa[i] = i, rank[i] = 0;
+}
+int find(int x)
+{
+	if (fa[x] == x) return x;
+	return fa[x] = find(fa[x]);
+}
+void unite(int x, int y)
+{
+	x = find(x); y = find(y);
+	if (x == y) return;
+	if (rank[x] < rank[y]) fa[x] = y;
+	else
+	{
+		fa[y] = x;
+		if (rank[x] == rank[y]) rank[x]++;
+	}
+}
+bool same(int x, int y)
+{
+	return find(x) == find(y);
+}
+bool connected(int n)
+{
+	int sum = 0;
+	for (int i = 1; i <= n; i++)
+		if (flag[i] && find(i) == i) sum++;
+	if (sum > 1) return false;
+	return true;
+}
+int main()
+{
+	int a, b;
+	int n = N - 2;
+	init(n);
+	memset(flag, false, sizeof(flag));
+	while (scanf("%d%d", &a, &b), ~a)
+	{
+		if (!a)
+		{
+			if (connected(n)) printf("Yes\n");
+			else printf("No\n");
+			init(n);
+			memset(flag, false, sizeof(flag));
+			continue;
+		}
+		if (same(a, b))
+		{
+			printf("No\n");
+			while (a) scanf("%d%d", &a, &b);
+			init(n);
+			memset(flag, false, sizeof(flag));
+			continue;
+		}
+		unite(a, b);
+		flag[a] = flag[b] = true;
+	}
+	return 0;
+}
---
+++ POJ/POJ 2236 Wireless Network (并查集).cpp
@@ -0,0 +1,74 @@
+﻿#include <cstdio>
+#include <cstring>
+#include <cmath>
+#define N 1010
+struct point
+{
+	int x, y;
+} computer[N];
+bool map[N][N], usable[N];
+int fa[N], rank[N];
+int n, d;
+double distance(point a, point b)
+{
+	int dx = a.x - b.x, dy = a.y - b.y;
+	return sqrt(dx * dx + dy * dy);
+}
+void init(int n)
+{
+	for (int i = 1; i <= n; i++) fa[i] = i, rank[i] = 0;
+}
+int find(int x)
+{
+	if (fa[x] == x) return x;
+	return fa[x] = find(fa[x]);
+}
+void unite(int x, int y)
+{
+	x = find(x); y = find(y);
+	if (x == y) return;
+	if (rank[x] < rank[y]) fa[x] = y;
+	else
+	{
+		fa[y] = x;
+		if (rank[x] == rank[y]) rank[x]++;
+	}
+}
+bool same(int x, int y)
+{
+	return find(x) == find(y);
+}
+int main()
+{
+	scanf("%d%d", &n, &d);
+	memset(map, false, sizeof(map));
+	for (int i = 1; i <= n; i++)
+	{
+		scanf("%d%d", &computer[i].x, &computer[i].y);
+		for (int j = 1; j < i; j++)
+			if (distance(computer[i], computer[j]) <= d) map[i][j] = map[j][i] = true;
+	}
+	getchar();
+	memset(usable, false, sizeof(usable));
+	init(n);
+	char c;
+	while (~(c = getchar()))
+	{
+		if (c == 'O')
+		{
+			int p;
+			scanf("%d%*c", &p);
+			usable[p] = true;
+			for (int i = 1; i <= n; i++)
+				if (map[p][i] && usable[i]) unite(p, i);
+		}
+		if (c == 'S')
+		{
+			int p, q;
+			scanf("%d%d%*c", &p, &q);
+			if (same(p, q)) printf("SUCCESS\n");
+			else printf("FAIL\n");
+		}
+	}
+	return 0;
+}
