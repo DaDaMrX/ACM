a31910163b235447e18c7bce26ad58c221672b6d
DaDaMrX <dadamrxx@gmail.com>
2017-04-16 21:32:20 +0800

    auto

--- Templates/Hungary.cpp
+++ Templates/Hungary.cpp
@@ -1,21 +1,49 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+const int N = 1e3 + 10; 
+const int M = 1e3 + 10; 
+ 
+struct Edge 
+{ 
+    int to, next; 
+} edge[M]; 
+int adj[N], no; 
+int n, m; 
+ 
+void init() 
+{
+	memset(adj, -1, sizeof(adj));
+	no = 0; 
+} 
+void add(int u, int v) 
+{ 
+	edge[no].to = v; 
+	edge[no].next = adj[u]; 
+	adj[u] = no++; 
+} 
+
 int left, right;
-bool g[N][N];
 int match[N];
 bool vis[N];
 bool dfs(int u)
 {
-	for (int v = 1; v <= right; v++)
-		if (g[u][v] && !vis[v])
+	for (int i = adj[u]; i != -1; i = edge[i].next)
+	{
+		Edge &e = edge[i];
+		if (vis[e.to]) continue;
+		if (match[e.to] == -1 || dfs(match[e.to]))
 		{
-			vis[v] = true;
-			if (match[v] < 0 || dfs(match[v]))
-			{
-				match[v] = u;
-				return true;
-			}
+			match[e.to] = u;
+			return true;
 		}
+	}
 	return false;
 }
+
 int hungary(int x, int y)
 {
 	left = x; right = y;
@@ -27,4 +55,20 @@ int hungary(int x, int y)
 		if (dfs(u)) ans++;
 	}
 	return ans;
+}
+
+int main()
+{
+	int n, m, e;
+	scanf("%d%d%d", &n, &m, &e);
+	init();
+	while (e--)
+	{
+		int u, v;
+		scanf("%d%d", &u, &v);
+		add(u, v);
+	}
+	int ans = hungary(n, m);
+	printf("%d\n", ans);
+	return 0;
 }
